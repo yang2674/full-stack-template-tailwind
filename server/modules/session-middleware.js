96e6af9fbe2428a3cf902cecac98ef30ccc4cd96
Avery Yang <averyyang2674@gmail.com>
2022-12-08 18:32:43 -0600

    autosave files

--- server/modules/session-middleware.js
+++ server/modules/session-middleware.js
@@ -14,23 +14,23 @@ const warnings = require('../constants/warnings');
 */
 
 const serverSessionSecret = () => {
-  if (
-    !process.env.SERVER_SESSION_SECRET ||
-    process.env.SERVER_SESSION_SECRET.length < 8 ||
-    process.env.SERVER_SESSION_SECRET === warnings.exampleBadSecret
-  ) {
-    // Warning if user doesn't have a good secret
-    console.log(warnings.badSecret);
-  }
+    if (
+        !process.env.SERVER_SESSION_SECRET ||
+        process.env.SERVER_SESSION_SECRET.length < 8 ||
+        process.env.SERVER_SESSION_SECRET === warnings.exampleBadSecret
+    ) {
+        // Warning if user doesn't have a good secret
+        console.log(warnings.badSecret);
+    }
 
-  return process.env.SERVER_SESSION_SECRET;
+    return process.env.SERVER_SESSION_SECRET;
 };
 
 module.exports = cookieSession({
-  secret: serverSessionSecret() || 'secret', // please set this in your .env file
-  key: 'user', // this is the name of the req.variable. 'user' is convention, but not required
-  resave: 'false',
-  saveUninitialized: false,
-  maxAge: 1000 * 60 * 60 * 24 * 7, // Set to 7 days - 1000ms * 60 seconds * 60 minutes * 24 hours * 7 days
-  secure: false,
+    secret: serverSessionSecret() || 'secret', // please set this in your .env file
+    key: 'user', // this is the name of the req.variable. 'user' is convention, but not required
+    resave: 'false',
+    saveUninitialized: false,
+    maxAge: 1000 * 60 * 60 * 24 * 7, // Set to 7 days - 1000ms * 60 seconds * 60 minutes * 24 hours * 7 days
+    secure: false,
 });
--- server/server.js
+++ server/server.js
@@ -32,5 +32,5 @@ const PORT = process.env.PORT || 5000;
 
 /** Listen * */
 app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
+    console.log(`Listening on port: ${PORT}`);
 });
